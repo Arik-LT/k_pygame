57bb12b3bc9c6ed6b55b55e15fcedd0dc9decdd2
Arik_Levy <ariklt96@gmail.com>
2021-04-29 21:52:05 +0200

    Clases_2

--- ball.py
+++ ball.py
@@ -2,18 +2,6 @@ import pygame as pg
 import sys
 from random import randint
 
-def rebotaX(x):
-    if x <= 0 or x >= ANCHO:
-        return -1
-    
-    return 1
-
-def rebotaY(y):
-    if y <= 0 or y >= ALTO:
-        return -1
-    
-    return 1
-
 
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
@@ -26,20 +14,44 @@ pg.init()
 pantalla = pg.display.set_mode((ANCHO, ALTO))
 reloj = pg.time.Clock()
 
-bolas = []
+class Bola():
+    def __init__(self, x, y ,vx = 5, vy = 5, color = (255, 255, 255), radio=10):
+        self.x = x 
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        self.radio = radio
 
+    def actualizar(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.y <= 0 or self.y >= ALTO:
+            self.vy = -self.vy
+    
+        if self.x <= 0 or self.x >= ANCHO:
+            self.vx = -self.vx
+
+    
+    def dibujar(self, pantalla):
+        pg.draw.circle(pantalla, self.color, (self.x, self.y), self.radio)
+
+
+ranges =[randint(-10, -5), randint(5, 10)]
+bolas = []
 for _ in range(10):
-    bola = {"x": randint(0, ANCHO),
-            "y": randint(0, ALTO),
-            "vx": randint(5, 10),
-            "vy": randint(5, 10),
-            "color": (randint(0, 255), randint(0, 255), randint(0, 255))
-        }
+    bola = Bola(randint(0, ANCHO), 
+                randint(0, ALTO),
+                (ranges[randint(0,1)]),
+                (ranges[randint(0,1)]),
+                (randint(0, 255), randint(0, 255), randint(0, 255)))
+
     bolas.append(bola)
 
 game_over = False
 while not game_over:
-    reloj.tick(60)
+    reloj.tick(120)
     # gestion de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
@@ -47,16 +59,12 @@ while not game_over:
 
     # Modificacion de estado
     for bola in bolas:
-        bola["x"] += bola["vx"]
-        bola["y"] += bola["vy"]
-
-        bola["vy"] *= rebotaY(bola["y"])
-        bola["vx"] *= rebotaX(bola["x"])
+        bola.actualizar()
 
     # Gestion de la pantalla
     pantalla.fill(BLACK)
     for bola in bolas:
-        pg.draw.circle(pantalla, bola["color"], (bola["x"], bola["y"]), 10)
+        bola.dibujar(pantalla)
 
     pg.display.flip()
 
